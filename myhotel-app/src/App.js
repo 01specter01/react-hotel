2cf515e45e66c4cc0793005b38165ec87764cbe9
Leo Volwahsen <volwahsenl@gmail.com>
2022-10-18 21:41:52 +0200

    hj

--- myhotel-app/src/App.js
+++ myhotel-app/src/App.js
@@ -6,19 +6,13 @@ function App() {
     return rooms.map((element, i) => {
         console.log(element);
         return (
-            <div
-                key={i.sort((a, b) => {
-                    if (a > b) return 1;
-                    else if (a < b) return -1;
-                    else return 0;
-                })}
-            >
+            <div key={i}>
                 <p>The RoomNumber is: {element.roomNo}</p>
                 <p>
                     The kustomer is called {element.guest.firstName}{" "}
                     {element.guest.lastName}
                 </p>
-                <p>The checkIn date: {element.checkIn}</p>
+                <p>The checkIn date: {element.checkIn.reverse()}</p>
                 <p>The checkIn date: {element.checkOut}</p>
                 <br></br>
             </div>
