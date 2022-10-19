2ac33d8ad5622fd63c00038b60729393116c3ad0
Leo Volwahsen <volwahsenl@gmail.com>
2022-10-19 21:09:03 +0200

    c

--- myhotel-app/src/App.js
+++ myhotel-app/src/App.js
@@ -1,5 +1,5 @@
 import rooms from "./rooms.json";
-// import "./App.css";
+import "./App.css";
 console.log(rooms);
 
 function App() {
@@ -7,14 +7,27 @@ function App() {
         .sort((a, b) => a.roomNo - b.roomNo)
         .map((element, i) => {
             return (
-                <div key={i}>
+                <div key={i} className="App">
                     <p>The RoomNumber is: {element.roomNo}</p>
                     <p>
-                        The kustomer is called {element.guest.firstName}{" "}
+                        The customer is called {element.guest.firstName}{" "}
                         {element.guest.lastName}
                     </p>
-                    <p>The checkIn date: {element.checkIn}</p>
-                    <p>The checkIn date: {element.checkOut}</p>
+                    <p>
+                        The checkIn date: {element.checkIn.slice(8)}
+                        {"-"}
+                        {element.checkIn.slice(5, 7)}
+                        {"-"}
+                        {element.checkIn.slice(0, 4)}
+                    </p>
+                    ss it to the Message component.
+                    <p>
+                        The checkOut date: {element.checkOut.slice(8)}
+                        {"-"}
+                        {element.checkOut.slice(5, 7)}
+                        {"-"}
+                        {element.checkOut.slice(0, 4)}
+                    </p>
                     <br></br>
                 </div>
             );
