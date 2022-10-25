40e9783739da15065c2bb4d4ef7f462fb394541e
Leo Volwahsen <volwahsenl@gmail.com>
2022-10-25 21:06:54 +0200

    4 done

--- myhotel-app/src/component/Form.js
+++ myhotel-app/src/component/Form.js
@@ -1,9 +1,7 @@
 import React from "react";
-
 export default class Form extends React.Component {
     constructor(props) {
         super(props);
-
         this.state = {
             firstname: "",
             lastname: "",
@@ -11,16 +9,25 @@ export default class Form extends React.Component {
             depaturedate: "",
         };
     }
-    handleChange(e) {
-        const target = e.target;
-        console.log(target);
-        const firstname = target.name;
-        console.log(firstname);
-        const lastname = target.name;
-        console.log(lastname);
+    handleChange(evt) {
+        const target = evt.target;
+        const name = target.name;
+        console.log(name);
         const value = target.value;
-
-        this.setState({ [lastname]: value });
+        console.log(value);
+        this.setState({ ...this.state, [name]: value });
+        const form = document.querySelector("form");
+        const inputf = document.querySelector("#F");
+        const inputl = document.querySelector("#L");
+        const inputAd = document.querySelector("#AD");
+        const inputDd = document.querySelector("#DD");
+        form.addEventListener("submit", () => {
+            evt.preventDefault();
+            localStorage.setItem("firstName", inputf.value);
+            localStorage.setItem("lastName", inputl.value);
+            localStorage.setItem("Anreise", inputAd.value);
+            localStorage.setItem("Abreise", inputDd.value);
+        });
     }
     render() {
         const { firstname, lastname, arivaldate, depaturedate } = this.state;
@@ -30,43 +37,46 @@ export default class Form extends React.Component {
                     <label>firstname: </label>
                     <input
                         type="text"
-                        name="One"
+                        name="firstname"
                         placeholder="firstname"
                         value={firstname}
                         onChange={this.handleChange.bind(this)}
+                        id="F"
                     />
                     <br></br>
                     <br></br>
                     <label>lastname: </label>
                     <input
                         type="text"
-                        name="name"
+                        name="lastname"
                         placeholder="lastname"
                         value={lastname}
                         onChange={this.handleChange.bind(this)}
+                        id="L"
                     />
                     <br></br>
                     <br></br>
-                    <label>arivaldate:</label>
+                    <label>arivaldate: </label>
                     <input
                         type="date"
-                        name="name"
-                        placeholder="arivaldate"
+                        name="arivaldate"
                         value={arivaldate}
                         onChange={this.handleChange.bind(this)}
+                        id="AD"
                     />
                     <br></br>
-                    <br></br>
-                    <label>depaturedate</label>
+                    <label>depaturedate: </label>
                     <input
                         type="date"
-                        name="name"
+                        name="depaturedate"
                         placeholder="depaturedate"
                         value={depaturedate}
                         onChange={this.handleChange.bind(this)}
+                        id="DD"
                     />
                     <br></br>
                     <br></br>
+                    <button type="submit">Senden</button>
                 </form>
             </div>
         );
